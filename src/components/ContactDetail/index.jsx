8056602c8eed13b0e98ea8dbc21f1dc8ad934a8d
Bruno Tiberio <quentemente@gmail.com>
2022-12-04 08:42:37 -0300

    feat: componente contactDetail

---
+++ src/components/ContactDetail/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function ContactDetail() {
+  return (
+    <>
+      <div>
+        <h2>Nome do contato</h2>
+        {/* map para email */}
+        <p>Ja apareceu o email do cara no p</p>
+        {/* map para telefones */}
+        <p>Ja apareceu os telefones do cara no p</p>
+      </div>
+      <div>
+        <button>Atualizar Contato</button>
+        <button>Excluir Contato</button>
+      </div>
+    </>
+  );
+}
+
+// usado para fazer o map dos contatos
