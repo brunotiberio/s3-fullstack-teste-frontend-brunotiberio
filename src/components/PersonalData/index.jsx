c135982d83132a4cfa34bdcde08d17dd27889bf4
Bruno Tiberio <quentemente@gmail.com>
2022-12-04 08:43:24 -0300

    feat: componente personalData

---
+++ src/components/PersonalData/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function PersonalData() {
+  return (
+    <>
+      <div>
+        <h1>Agenda</h1>
+        <div>
+          <h2>Olá, fullname</h2>
+        </div>
+        <div>
+          <button>Alterar meus dados</button>
+          <button>Adicionar novo contato</button>
+          <button>Sair</button>
+        </div>
+      </div>
+    </>
+  );
+}
