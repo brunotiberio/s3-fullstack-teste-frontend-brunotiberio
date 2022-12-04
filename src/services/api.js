a73438ea3597c8dd051f4e735bcde531a31c9192
Bruno Tiberio <quentemente@gmail.com>
2022-12-04 09:35:20 -0300

    feat: token localstorage

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+const token = localStorage.getItem("@AGENDA-TOKEN");
+
 export const api = axios.create({
   baseURL: "http://localhost:3000/",
+  headers: { Authorization: "Bearer" + token },
   timeout: 8000,
 });
