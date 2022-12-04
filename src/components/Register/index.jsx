5fadca7eb968ea0053986a43b308a3ad1c8bcc65
Bruno Tiberio <quentemente@gmail.com>
2022-12-04 19:55:46 -0300

    feat: criação componente register

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,30 +1,75 @@
-import React from "react";
+import React, { useContext } from "react";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import { UserContext } from "../../contexts/UserContext";
 
 export default function Register() {
+  const { userCreate } = useContext(UserContext);
+
+  const formSchema = yup.object().shape({
+    fullname: yup.string().required("Nome é obrigatório"),
+    username: yup.string().required("Nome de usuário é obrigatório"),
+    password: yup.string().required("Senha é obrigatória"),
+    emails: yup.string().required("Email obrigatório"),
+    phones: yup.string().required("Telefone obrigatório"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  });
+
+  const onSubmit = (data) => {
+    const formData = data;
+    formData.emails = formData.emails.replace(" ", "").split(",");
+    formData.phones = formData.phones.replace(" ", "").split(",");
+    console.log(formData);
+    userCreate(formData);
+  };
+
   return (
     <>
       <h2>Cadastro</h2>
-      <input type="text" placeholder="Nome Completo" />
-      <input type="text" placeholder="Username" />
-      <input type="text" placeholder="Senha" />
-      <div>
-        <div>
-        <input type="text" placeholder="Email" />
-        </div>
-        <button>Adicionar Email</button>
-      </div>
-      <div>
-        <div>
-        <input type="text" placeholder="Telefone" />
-        </div>
-        <button>Adicionar Telefone</button>
-      </div>
-      <button>Cadastrar</button>
-      <div>
-      <p>Já possui cadastro? Faça o</p>
-      <button>Login</button>
-      </div>
-      
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="Nome Completo"
+          {...register("fullname")}
+        />
+        {errors.fullname?.message && <p>Digite seu nome completo</p>}
+        <input
+          type="text"
+          placeholder="Nome de usuário"
+          {...register("username")}
+        />
+        {errors.username?.message && <p>Nome de usuário é obrigatório</p>}
+        <input type="password" placeholder="Senha" {...register("password")} />
+        {errors.password?.message && <p>Digite uma senha</p>}
+        <input
+          type="text"
+          placeholder="Coloque seus e-mails (separe por vírgula)"
+          {...register("emails")}
+        />
+        {errors.emails?.message && (
+          <p>Coloque um ou mais e-mails separados por vírgula</p>
+        )}
+        <input
+          type="text"
+          placeholder="Coloque seus telefones (separe por vírgula)"
+          {...register("phones")}
+        />
+        {errors.phones?.message && (
+          <p>Coloque um ou mais telefones separados por vírgula</p>
+        )}
+        <button>Cadastrar</button>
+      </form>
+      <p>
+        Já possui cadastro? Faça o <button>Login</button>
+      </p>
     </>
   );
 }
