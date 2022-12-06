1e585f6bcf7005d7acedf99a5b960312c8d934c8
Bruno Tiberio <quentemente@gmail.com>
2022-12-05 22:56:08 -0300

    feat: pages e componentes de contact

--- src/components/ContactDetail/index.jsx
+++ src/components/ContactDetail/index.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ContactsContext } from "../../contexts/ContactsContext";
 
 export default function ContactDetail() {
+  const { contact } = useContext(ContactsContext);
+
   return (
     <>
       <div>
-        <h2>Nome do contato</h2>
-        {/* map para email */}
-        <p>Ja apareceu o email do cara no p</p>
-        {/* map para telefones */}
-        <p>Ja apareceu os telefones do cara no p</p>
+        <h2>{contact?.fullname}</h2>
+        <ul> Email para contato
+          {contact?.emails.length === 0 ? (
+            <li>Atualize para adicionar emails</li>
+          ) : (
+            contact.emails.map((email, index) => <li key={index}>{email}</li>)
+          )}
+        </ul>
+        <ul> Telefone para contato
+          {contact?.phones.length === 0 ? (
+            <li>Atualize para adicionar telefones</li>
+          ) : (
+            contact.phones.map((phone, index) => <li key={index}>{phone}</li>)
+          )}
+        </ul>
       </div>
       <div>
         <button>Atualizar Contato</button>
---
+++ src/pages/ContactDetailPage/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import ContactDetail from "../../components/ContactDetail";
+import Header from "../../components/Header";
+import Main from "../../components/Main";
+
+export default function ContactDetailPage() {
+  const history = useHistory()
+
+  return (
+    <>
+      <Header>
+        <h1>Agenda</h1>
+      </Header>
+      <Main>
+      <ContactDetail />
+      <button onClick={() => history.push('/user')}>Voltar para a página principal</button>
+      </Main>
+    </>
+  );
+}
