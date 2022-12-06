6f080d20750d8a47bc2c44d361cf4e501cfca10f
Bruno Tiberio <quentemente@gmail.com>
2022-12-06 13:41:50 -0300

    fix: imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,4 @@
 import { StyledGlobal } from './styles/global';
-import Header from './components/Header';
-import Main from './components/Main';
-import PersonalData from './components/PersonalData';
-import Contacts from './components/Contacts';
-import ContactDetail from './components/ContactDetail';
-import UpdateContact from './components/UpdateContact';
-import Register from './components/Register';
-import Login from './components/Login';
 import Paths from './routes';
 
 function App() {
