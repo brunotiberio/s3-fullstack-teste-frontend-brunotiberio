6f3a72d4b3394582277fce101e47ed19aae0e6c6
Bruno Tiberio <quentemente@gmail.com>
2022-12-06 10:28:01 -0300

    feat: createContact page

---
+++ src/pages/CreateContactPage/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CreateContact from '../../components/CreateContact'
+
+export default function CreateContactPage(){
+    return (
+        <>
+            <h1>Agenda</h1>
+            <CreateContact />
+        </>
+    )
+}
