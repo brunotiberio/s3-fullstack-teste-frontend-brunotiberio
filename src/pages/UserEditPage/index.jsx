7117f43edb18762ae634cd2928bef5c368bb6b6c
Bruno Tiberio <quentemente@gmail.com>
2022-12-07 16:14:59 -0300

    fix: remove de arquivo configuração favicon e html

--- src/pages/UserEditPage/index.jsx
+++ src/pages/UserEditPage/index.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
-import Header from '../../components/Header'
-import Main from '../../components/Main'
-import UserEdit from '../../components/UserEdit'
-import { StyledH1EditUser, StyledH2EditUser } from './style'
+import React from "react";
+import Header from "../../components/Header";
+import Main from "../../components/Main";
+import UserEdit from "../../components/UserEdit";
+import { StyledH1EditUser, StyledH2EditUser } from "./style";
 
-export default function UserEditPage(){
-    
-    
-    return (
-        <>
-        <Header>
-            <StyledH1EditUser>Agenda</StyledH1EditUser>
-        </Header>
-        <Main>
-            <StyledH2EditUser>Editar perfil</StyledH2EditUser>
-            <UserEdit />
-        </Main>
-        </>
-    )
+export default function UserEditPage() {
+  return (
+    <>
+      <Header>
+        <StyledH1EditUser>Agenda</StyledH1EditUser>
+      </Header>
+      <Main>
+        <StyledH2EditUser>Editar perfil</StyledH2EditUser>
+        <UserEdit />
+      </Main>
+    </>
+  );
 }
