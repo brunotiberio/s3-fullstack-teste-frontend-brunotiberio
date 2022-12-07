0f4475514eb283d70617485fbcdce86a91eecd64
Bruno Tiberio <quentemente@gmail.com>
2022-12-07 14:51:13 -0300

    feat: estilização contactDetail

---
+++ src/components/ContactDetail/style.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const StyledH2ContactDetail = styled.h2`
+  color: white;
+  text-align: center;
+
+  margin-top: 0.5rem;
+`;
+
+export const StyledDivContainerContactDetail = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+
+  margin-bottom: 10px;
+
+  ul{
+    list-style: none;
+    color: white;
+    text-align: center;
+    margin-bottom: 20px;
+
+    li{
+        text-align: center;
+        margin-top: 10px;
+    }
+  }
+`;
