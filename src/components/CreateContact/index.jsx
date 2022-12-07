d2a409191282eec68363896496127b8dcbd92d52
Bruno Tiberio <quentemente@gmail.com>
2022-12-07 14:14:03 -0300

    feat: estilização createContact

--- src/components/CreateContact/index.jsx
+++ src/components/CreateContact/index.jsx
@@ -5,16 +5,22 @@ import { useForm } from "react-hook-form";
 import { useHistory } from "react-router-dom";
 import { ContactsContext } from "../../contexts/ContactsContext";
 import { UserContext } from "../../contexts/UserContext";
+import {
+  StyledH2ContactCreate,
+  StyledContainerInputsContactCreate,
+  StyledContainerContactCreate,
+  StyledButtonContainerContactCreate,
+} from "./style";
 
 export default function CreateContact() {
   const { contactCreate } = useContext(ContactsContext);
   const { getUser } = useContext(UserContext);
-  const [contactCreated, setContactCreated] = useState(false)
-  const history = useHistory()
+  const [contactCreated, setContactCreated] = useState(false);
+  const history = useHistory();
 
   useEffect(() => {
-    getUser() // eslint-disable-next-line
-  }, [contactCreated])
+    getUser(); // eslint-disable-next-line
+  }, [contactCreated]);
 
   const formSchema = yup.object().shape({
     fullname: yup.string().required("Nome é obrigatório"),
@@ -34,43 +40,50 @@ export default function CreateContact() {
     const formData = data;
     formData.emails = formData.emails.replace(" ", "").split(",");
     formData.phones = formData.phones.replace(" ", "").split(",");
-    setContactCreated(!contactCreated)
+    setContactCreated(!contactCreated);
     contactCreate(formData);
   };
 
   return (
     <>
-      <h2>Adicionar contato</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <StyledH2ContactCreate>Adicionar contato</StyledH2ContactCreate>
+      <StyledContainerInputsContactCreate>
+        <StyledContainerContactCreate>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+              type="text"
+              placeholder="Nome Completo"
+              {...register("fullname")}
+            />
+            {errors.fullname?.message && <p>Digite seu nome completo</p>}
 
-        <input
-          type="text"
-          placeholder="Nome Completo"
-          {...register("fullname")}
-        />
-        {errors.fullname?.message && <p>Digite seu nome completo</p>}
+            <input
+              type="text"
+              placeholder="Coloque os e-mails do contato (separe por vírgula)"
+              {...register("emails")}
+            />
+            {errors.emails?.message && (
+              <p>Coloque um ou mais e-mails separados por vírgula</p>
+            )}
 
-        <input
-          type="text"
-          placeholder="Coloque seus e-mails (separe por vírgula)"
-          {...register("emails")}
-        />
-        {errors.emails?.message && (
-          <p>Coloque um ou mais e-mails separados por vírgula</p>
-        )}
+            <input
+              type="text"
+              placeholder="Coloque os telefones do contato (separe por vírgula)"
+              {...register("phones")}
+            />
+            {errors.phones?.message && (
+              <p>Coloque um ou mais telefones separados por vírgula</p>
+            )}
 
-        <input
-          type="text"
-          placeholder="Coloque seus telefones (separe por vírgula)"
-          {...register("phones")}
-        />
-        {errors.phones?.message && (
-          <p>Coloque um ou mais telefones separados por vírgula</p>
-        )}
-
-        <button>Adicionar</button>
-      </form>
-      <button onClick={() => history.push('/user')}>Voltar a página principal</button>
+            <button>Adicionar</button>
+          </form>
+        </StyledContainerContactCreate>
+      </StyledContainerInputsContactCreate>
+      <StyledButtonContainerContactCreate>
+      <button onClick={() => history.push("/user")}>
+        Voltar a página principal
+      </button>
+      </StyledButtonContainerContactCreate>
     </>
   );
 }
---
+++ src/components/CreateContact/style.js
@@ -0,0 +1,95 @@
+import styled from "styled-components";
+
+export const StyledH2ContactCreate = styled.h2`
+  color: white;
+  text-align: center;
+
+  margin-top: 0.5rem;
+`;
+
+export const StyledContainerInputsContactCreate = styled.div`
+  width: 70vw;
+  max-width: 70vw;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+
+  margin: auto;
+  margin-top: 10px;
+`;
+
+export const StyledContainerContactCreate = styled.div`
+  background-color: #0c6885;
+  border-radius: 20px 20px;
+  box-shadow: 5px 10px 10px white;
+
+  min-width: 400px;
+  margin: auto;
+
+  margin-bottom: 20px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    width: 100%;
+
+    input {
+      margin-top: 10px;
+      width: 360px;
+      min-width: 150px;
+      max-width: 360px;
+
+      height: 50px;
+
+      background-color: white;
+
+      border: 2px solid #857c13;
+      border-radius: 20px 20px;
+
+      text-align: center;
+    }
+
+    button {
+      margin: auto;
+      margin-top: 10px;
+      margin-bottom: 10px;
+      border: 2px solid #383405;
+      border-radius: 20px 20px;
+
+      width: 50%;
+      height: 40px;
+    }
+  }
+
+  p {
+    color: #38101c;
+    font-weight: 500;
+    margin-left: 5%;
+
+    font-size: 12px;
+  }
+`;
+
+export const StyledButtonContainerContactCreate = styled.div`
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+
+  button {
+    margin-bottom: 10px;
+    margin-left: 10px;
+    border: 2px solid #383405;
+    border-radius: 20px 20px;
+
+    width: 20%;
+    height: 40px;
+  }
+`;
